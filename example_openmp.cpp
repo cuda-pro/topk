6208fd8df4f50e78ea70deb19f95d149e3b17c65
weedge <weege007@gmail.com>
2023-11-16 21:11:15 +0800

    change
    Signed-off-by: weedge <weege007@gmail.com>

--- example_openmp.cpp
+++ example_openmp.cpp
@@ -1,20 +1,25 @@
 // g++ -fopenmp example_openmp.cpp -o bin/example_openmp --std=c++11 -O3
+/*
+- https://lemon-412.github.io/imgs/20200516OpenMP_simple_Program.pdf
+- https://scc.ustc.edu.cn/zlsc/cxyy/200910/W020121113517997951933.pdf
+- https://openmpusers.org/wp-content/uploads/uk-openmp-users-2018-OpenMP45Tutorial_new.pdf
+- https://enccs.github.io/openmp-gpu/
+- https://www.openmp.org/resources/openmp-books/
+
+please Think for yourself
+please Think for yourself
+please Think for yourself
+*/
 
 #include <omp.h>
 
+#include <chrono>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int g_ncore = omp_get_num_procs();  // 获取执行核的数量
 
-/** 计算循环迭代需要的线程数量
-根据循环迭代次数和 CPU 核数及一个线程最少需要的循环迭代次数
-来计算出需要的线程数量，计算出的最大线程数量丌超过 CPU 核数
-@param int n - 循环迭代次数
-@param int min_n - 单个线程需要的最少迭代次数
-@return int - 线程数量
-*/
 int dtn(int n, int min_n) {
     int max_tn = n / min_n;
     int tn = max_tn > g_ncore ? g_ncore : max_tn;  // tn 表示要设置的线程数量
@@ -24,17 +29,6 @@ int dtn(int n, int min_n) {
     return tn;
 }
 
-/** 矩阵串行乘法函数
-@param int *a - 指向要相乘的第个矩阵的指针
-@param int row_a - 矩阵 a 的行数
-@param int col_a - 矩阵 a 的列数
-@param int *b - 指向要相乘的第个矩阵的指针
-@param int row_b - 矩阵 b 的行数
-@param int col_b - 矩阵 b 的列数
-@param int *c - 计算结果的矩阵的指针
-@param int c_size - 矩阵 c 的空间大小（总元素个数）
-@return void - 无
-*/
 void Matrix_Multiply(int *a, int row_a, int col_a,
                      int *b, int row_b, int col_b,
                      int *c, int c_size) {
@@ -42,7 +36,7 @@ void Matrix_Multiply(int *a, int row_a, int col_a,
         return;
     }
     int i, j, k;
-#pragma omp for private(i, j, k)
+    // #pragma omp for private(i, j, k)
     for (i = 0; i < row_a; i++) {
         int row_i = i * col_a;
         int row_c = i * col_b;
@@ -58,7 +52,7 @@ void Matrix_Multiply(int *a, int row_a, int col_a,
 void Parallel_Matrix_Multiply(int *a, int row_a, int col_a,
                               int *b, int row_b, int col_b,
                               int *c, int c_size) {
-    if (col_a != row_b) {
+    if (col_a != row_b || c_size < row_a * col_b) {
         return;
     }
     int i, j, k;
@@ -80,35 +74,164 @@ void Parallel_Matrix_Multiply(int *a, int row_a, int col_a,
 }
 
 void case1() {
-    std::vector<int> a(10);
+    std::vector<int> a(1000000);
     int start_doc_id = 10;
-    omp_set_num_threads(2);
-#pragma omp parallel for schedule(static)
-    for (int i = 0; i < a.size(); i++) {
-        a[i] = i + start_doc_id;
-        cout << omp_get_thread_num() << endl;
+    // omp_set_num_threads(2);
+#pragma omp parallel for
+    for (int i = 0; i < 10000000; i++) {
+        // a[i] = i + start_doc_id;
+        //  cout << omp_get_thread_num() << endl;
     }
-
-    for (auto i : a) {
-        cout << i << ",";
-    }
-    cout << endl;
+    std::cout << a.size() << std::endl;
 }
 
 void case2() {
     int i, j;
     int a[100][100] = {0};
-#pragma omp parallel for schedule(dynamic)
+#pragma omp parallel for schedule(static)
     for (i = 0; i < 100; i++) {
         for (j = i; j < 100; j++) {
             a[i][j] = i * j;
-            printf("a[%d][%d]=%d, tn=%d\n", i, j, a[i][j], omp_get_thread_num());
+            // printf("a[%d][%d]=%d, tn=%d\n", i, j, a[i][j], omp_get_thread_num());
         }
     }
 }
 
+void case3() {
+    int a[2][3] = {
+        {1, 2, 3},
+        {1, 2, 3},
+    };
+    int b[3][2] = {
+        {1, 2},
+        {1, 2},
+        {1, 2},
+    };
+    int *c = new int[2 * 2];
+
+#ifdef PARALLEL_MATRIX_MULTIPLY
+    Parallel_Matrix_Multiply(a[0], 2, 3, b[0], 3, 2, c, 4);
+#else
+    Matrix_Multiply(a[0], 2, 3, b[0], 3, 2, c, 4);
+#endif
+
+    for (int i = 0; i < 4; i++) {
+        printf("%d,", c[i]);
+    }
+
+    free(c);
+}
+
+#define N 100000000
+
+int fun() {
+    int a = 0;
+    for (int i = 0; i < N; ++i) {
+        a += i;
+    }
+    return a;
+}
+void case4() {
+#pragma omp parallel for num_threads(2) schedule(dynamic)
+    for (int i = 0; i < 100; ++i) {
+        fun();
+    }
+    std::cout << "finish" << std::endl;
+}
+
+void test() {
+    int a = 0;
+    double t1, t2;
+    t1 = omp_get_wtime();
+    for (int i = 0; i < 100000000; i++) {
+        a = i + 1;
+    }
+    t2 = omp_get_wtime();
+    printf("Time = %f s\n", t2 - t1);
+}
+void case5() {
+    double t1, t2;
+    t1 = omp_get_wtime();
+#pragma omp parallel for
+    for (int j = 0; j < 2; j++) {
+        test();
+    }
+    t2 = omp_get_wtime();
+    printf("Total time = %f s\n", t2 - t1);
+    test();
+}
+
+/* Seriel Code */
+static long num_steps = 100000;
+double step;
+void pi_serial() {
+    int i;
+    double x, pi, sum = 0.0, start_time, end_time;
+    step = 1.0 / (double)num_steps;
+    // start_time = clock();
+    start_time = omp_get_wtime();
+    for (i = 1; i <= num_steps; i++) {
+        x = (i - 0.5) * step;
+        sum = sum + 4.0 / (1.0 + x * x);
+    }
+    pi = step * sum;
+    // end_time = clock();
+    end_time = omp_get_wtime();
+    printf("Pi = %f\t pi_serial Running time %f s\n", pi, end_time - start_time);
+}
+
+#define NUM_THREADS 4
+void pi_parallel() {
+    int i;
+    double pi, sum[NUM_THREADS], start_time, end_time;
+    step = 1.0 / (double)num_steps;
+    omp_set_num_threads(NUM_THREADS);
+    // start_time = clock();
+    start_time = omp_get_wtime();
+#pragma omp parallel
+    {
+        int id;
+        double x;
+        id = omp_get_thread_num();
+        for (i = id, sum[id] = 0.0; i < num_steps; i = i + NUM_THREADS) {
+            x = (i + 0.5) * step;
+            sum[id] += 4.0 / (1.0 + x * x);
+        }
+    }
+    for (i = 0, pi = 0.0; i < NUM_THREADS; i++) pi += sum[i] * step;
+    // end_time = clock();
+    end_time = omp_get_wtime();
+    printf("Pi = %f\t parallel Running time %f s\n", pi, end_time - start_time);
+}
+
+void pi_parallel_for() {
+    int i, id;
+    double x, pi, sum[NUM_THREADS], start_time, end_time;
+    step = 1.0 / (double)num_steps;
+    omp_set_num_threads(NUM_THREADS);
+    // start_time = clock();
+    start_time = omp_get_wtime();
+#pragma omp parallel private(x, id)
+    {
+        id = omp_get_thread_num();
+        sum[id] = 0;
+#pragma omp for
+        for (i = 0; i < num_steps; i++) {
+            x = (i + 0.5) * step;
+            sum[id] += 4.0 / (1.0 + x * x);
+        }
+    }
+    for (i = 0, pi = 0.0; i < NUM_THREADS; i++) pi += sum[i] * step;
+    // end_time = clock();
+    end_time = omp_get_wtime();
+    printf("Pi = %f\t parallel for Running time %f s\n", pi, end_time - start_time);
+}
+
 int main() {
-    case2();
+    case5();
+    pi_serial();
+    pi_parallel();
+    pi_parallel_for();
 
     return 0;
 }
